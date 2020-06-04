a5967ecee765400196f86c9546783afe602782e1
niki-theWack <60672819+niki-theWack@users.noreply.github.com>
2020-06-03 21:45:31 -0700

    hj

--- Space.java
+++ Space.java
@@ -21,20 +21,21 @@ public class Space extends World
         GreenfootImage background = getBackground();
         background.setColor(Color.BLACK);
         background.fill();
-        
+
         Rocket rocket = new Rocket();
         addObject(rocket, getWidth()/2 + 100, getHeight()/2);
-        
+
         addAsteroids(startAsteroids);
         paintStars(300);
-        
+
         scoreCounter = new Counter("Score: ");
         addObject(scoreCounter, 60, 480);
 
         Explosion.initializeImages();
         ProtonWave.initializeImages();
+        prepare();
     }
-    
+
     /**
      * Add a given number of asteroids to our world. Asteroids are only added into
      * the left half of the world.
@@ -48,12 +49,12 @@ public class Space extends World
             addObject(new Asteroid(), x, y);
         }
     }
-    
+
     public void updateScore(int addToScore)
     {
         scoreCounter.add(addToScore);
     }
-    
+
     private void paintStars(int count)
     {
         GreenfootImage background = getBackground();
@@ -61,27 +62,27 @@ public class Space extends World
         {
             int x = Greenfoot.getRandomNumber(getWidth());
             int y = Greenfoot.getRandomNumber(getHeight());
-            
+
             int deltaRed = Greenfoot.getRandomNumber(80) - 40;
             int deltaGreen = Greenfoot.getRandomNumber(80) - 40;
             int deltaBlue = Greenfoot.getRandomNumber(80) - 40;
-            
+
             int starBrightness = Greenfoot.getRandomNumber(120) + 80;
-            
+
             Color StarColor = new Color(starBrightness + deltaRed, starBrightness + deltaGreen, starBrightness + deltaBlue);
-            
+
             int starSize = Greenfoot.getRandomNumber(4) + 1;
-            
+
             background.setColor(StarColor);
             background.fillOval(x, y, starSize,starSize);
-         
+
         }
-        
-        }
-   
-        /**
-         * This method is called when the game is over to display the final score.
-         */
+
+    }
+
+    /**
+     * This method is called when the game is over to display the final score.
+     */
     public void gameOver() 
     {
         // TODO: show the score board here. Currently missing.
@@ -91,6 +92,23 @@ public class Space extends World
         addObject(new ScoreBoard(currentScore), x, y);
     }
 
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Bee bee = new Bee();
+        addObject(bee,521,69);
+        Bee bee2 = new Bee();
+        addObject(bee2,90,376);
+        Bee bee3 = new Bee();
+        addObject(bee3,530,415);
+        Fire fire = new Fire();
+        addObject(fire,45,50);
+        Fire fire2 = new Fire();
+        addObject(fire2,310,424);
+    }
 }
 
 
